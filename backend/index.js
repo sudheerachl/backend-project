b843e501d1497fbc8faf4f92dd2c49752a0a7eec
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-11 23:15:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const FormDataModel = require ('./models/FormData');
+const DoctorModel = require ('./models/Doctordata');
+const UserModel = require ('./models/UserData');
 
 
 const app = express();
@@ -10,28 +11,66 @@ app.use(cors());
 
 mongoose.connect('mongodb+srv://saisudheera9803:Sai2344557@cluster0.gsisntp.mongodb.net/');
 
-app.post('/register', (req, res)=>{
+app.post('/signup-doctor', (req, res)=>{
     // To post / insert data into database...
 
     const {email, password} = req.body;
-    FormDataModel.findOne({email: email})
+    DoctorModel.findOne({email: email})
     .then(user => {
         if(user){
             res.json("Already registered")
         }
         else{
-            FormDataModel.create(req.body)
-            .then(log_reg_form => res.json(log_reg_form))
+            DoctorModel.create(req.body)
+            .then(doctor => res.json(doctor))
             .catch(err => res.json(err))
         }
     })
     
 })
 
-app.post('/login', (req, res)=>{
+app.post('/login-doctor', (req, res)=>{
     // To find record from the database
     const {email, password} = req.body;
-    FormDataModel.findOne({email: email})
+    DoctorModel.findOne({email: email})
+    .then(user => {
+        if(user){
+            // If user found then these 2 cases
+            if(user.password === password) {
+                res.json("Success");
+            }
+            else{
+                res.json("Wrong password");
+            }
+        }
+        // If user not found then 
+        else{
+            res.json("No records found! ");
+        }
+    })
+})
+app.post('/signup-user', (req, res)=>{
+    // To post / insert data into database...
+
+    const {email, password} = req.body;
+   UserModel.findOne({email: email})
+    .then(user => {
+        if(user){
+            res.json("Already registered")
+        }
+        else{
+            UserModel.create(req.body)
+            .then(doctor => res.json(doctor))
+            .catch(err => res.json(err))
+        }
+    })
+    
+})
+
+app.post('/login-user', (req, res)=>{
+    // To find record from the database
+    const {email, password} = req.body;
+    UserModel.findOne({email: email})
     .then(user => {
         if(user){
             // If user found then these 2 cases
