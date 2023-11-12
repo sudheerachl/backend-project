fc260449bb58db54a46642ffc8246b218d961c30
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-12 08:53:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const DoctorModel = require ('./models/Doctordata');
-const UserModel = require ('./models/UserData');
-
+const DoctorModel = require('./models/DoctorData');
+const UserModel = require('./models/UserData');
 
 const app = express();
 app.use(express.json());
@@ -11,84 +10,78 @@ app.use(cors());
 
 mongoose.connect('mongodb+srv://saisudheera9803:Sai2344557@cluster0.gsisntp.mongodb.net/');
 
-app.post('/signup-doctor', (req, res)=>{
-    // To post / insert data into database...
+// Doctor Signup
+app.post('/signup-doctor', (req, res) => {
+  const { email, password } = req.body;
 
-    const {email, password} = req.body;
-    DoctorModel.findOne({email: email})
-    .then(doctor => {
-        if(doctor){
-            res.json("Already registered")
-        }
-        else{
-            DoctorModel.create(req.body)
-            .then(doctor => res.json(doctor))
-            .catch(err => res.json(err))
-        }
-    })
-    
-})
+  DoctorModel.findOne({ email }).then((doctor) => {
+    if (doctor) {
+      res.json('Doctor already registered');
+      return;
+    }
 
-app.post('/login-doctor', (req, res)=>{
-    // To find record from the database
-    const {email, password} = req.body;
-    DoctorModel.findOne({email: email})
-    .then(doctor => {
-        if(doctor){
-            // If user found then these 2 cases
-            if(doctor.password === password) {
-                res.json("Success");
-            }
-            else{
-                res.json("Wrong password");
-            }
-        }
-        // If user not found then 
-        else{
-            res.json("No records found! ");
-        }
-    })
-})
-app.post('/signup-user', (req, res)=>{
-    // To post / insert data into database...
+    DoctorModel.create(req.body).then((newDoctor) => {
+      res.json(newDoctor);
+    }).catch((err) => {
+      res.json(err);
+    });
+  });
+});
 
-    const {email, password} = req.body;
-   UserModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            res.json("Already registered")
-        }
-        else{
-            UserModel.create(req.body)
-            .then(doctor => res.json(doctor))
-            .catch(err => res.json(err))
-        }
-    })
-    
-})
+// Doctor Login
+app.post('/login-doctor', (req, res) => {
+  const { email, password } = req.body;
 
-app.post('/login-user', (req, res)=>{
-    // To find record from the database
-    const {email, password} = req.body;
-    UserModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            // If user found then these 2 cases
-            if(user.password === password) {
-                res.json("Success");
-            }
-            else{
-                res.json("Wrong password");
-            }
-        }
-        // If user not found then 
-        else{
-            res.json("No records found! ");
-        }
-    })
-})
+  DoctorModel.findOne({ email }).then((doctor) => {
+    if (!doctor) {
+      res.json('Doctor not found');
+      return;
+    }
+
+    if (doctor.password === password) {
+      res.json('Login successful');
+    } else {
+      res.json('Incorrect password');
+    }
+  });
+});
+
+// User Signup
+app.post('/signup-user', (req, res) => {
+  const { email, password } = req.body;
+
+  UserModel.findOne({ email }).then((user) => {
+    if (user) {
+      res.json('User already registered');
+      return;
+    }
+
+    UserModel.create(req.body).then((newUser) => {
+      res.json(newUser);
+    }).catch((err) => {
+      res.json(err);
+    });
+  });
+});
+
+// User Login
+app.post('/login-user', (req, res) => {
+  const { email, password } = req.body;
+
+  UserModel.findOne({ email }).then((user) => {
+    if (!user) {
+      res.json('User not found');
+      return;
+    }
+
+    if (user.password === password) {
+      res.json('Login successful');
+    } else {
+      res.json('Incorrect password');
+    }
+  });
+});
 
 app.listen(3001, () => {
-    console.log("Server listining on http://127.0.0.1:3001");
-
+  console.log('Server listening on http://localhost:3001');
 });
