0f4d71c392b7b2ca14add2c1a617b220bc269643
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-16 10:01:18 +0530

    Update UserData.js

--- backend/models/UserData.js
+++ backend/models/UserData.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name : String,
-    email: String,
-    password: String
-})
+  name: String,
+  username: String,
+  phoneNumber: String,
+  gender: String,
+  email: String,
+  password: String
+});
 
 const UserModel = mongoose.model('User', UserSchema);
 
