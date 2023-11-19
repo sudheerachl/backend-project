0ba55af11a96db153084856b753c88f8610d173e
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-19 12:24:13 +0530

    Update Doctordata.js

--- backend/models/Doctordata.js
+++ backend/models/Doctordata.js
@@ -8,7 +8,40 @@ const DoctorSchema = new mongoose.Schema({
   email: String,
   password: String
 });
+// Define user schema
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  gender: { type: String, required: true },
+});
 
+// Create user model
 const DoctorModel = mongoose.model('Doctor',DoctorSchema);
+
+// Middleware for JWT authentication
+const authMiddleware = async (req, res, next) => {
+  const token = req.headers['authorization'].split(' ')[1];
+  if (!token) {
+    return res.status(401).send('Unauthorized');
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decodedToken.userId;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(401).send('Unauthorized');
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).send('Unauthorized');
+  }
+};
+
+
 
 module.exports = DoctorModel;
